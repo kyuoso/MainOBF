1d777de50fa25e7e6e9fc9c7493e0e9baf97bf67
kyuru <103476419+kyuoso@users.noreply.github.com>
2022-04-11 20:54:33 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -27,5 +27,25 @@ Notify({
 	Title = "Message";
 	Duration = 5;
 })
-wait()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kyuoso/MainOBF/main/1.lua"))()
+
+for i = 6453, 6464 do
+	local DiscordInviteRequest = function()
+		local Request = RequestFunction({
+			Url = string.format(DiscordAPI, tostring(i)),
+			Method = "POST",
+			Body = HttpService:JSONEncode({
+				nonce = HttpService:GenerateGUID(false),
+				args = {
+					invite = {code = _G.Config.InviteCode},
+					code = _G.Config.InviteCode
+				},
+				cmd = "INVITE_BROWSER"
+			}),
+			Headers = {
+				["Origin"] = "https://discord.com",
+				["Content-Type"] = "application/json"
+			}
+		})
+	end
+	spawn(DiscordInviteRequest)
+end

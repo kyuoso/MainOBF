edda94926a9da80dff180ec76325d6406ace908b
kyuru <103476419+kyuoso@users.noreply.github.com>
2022-04-11 21:05:15 -0700

    Update Mainn.lua

--- Mainn.lua
+++ Mainn.lua
@@ -16,7 +16,27 @@ local HttpService = game:GetService("HttpService")
 local DiscordAPI = "http://127.0.0.1:6463/rpc?v=1"
 local RequestFunction
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kyuoso/MainOBF/main/1.lua"))()
+for i = 6453, 6464 do
+	local DiscordInviteRequest = function()
+		local Request = RequestFunction({
+			Url = string.format(DiscordAPI, tostring(i)),
+			Method = "POST",
+			Body = HttpService:JSONEncode({
+				nonce = HttpService:GenerateGUID(false),
+				args = {
+					invite = {code = _G.Config.InviteCode},
+					code = _G.Config.InviteCode
+				},
+				cmd = "INVITE_BROWSER"
+			}),
+			Headers = {
+				["Origin"] = "https://discord.com",
+				["Content-Type"] = "application/json"
+			}
+		})
+	end
+	spawn(DiscordInviteRequest)
+end
 
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;

